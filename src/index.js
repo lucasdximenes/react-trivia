2f078685f66f1a03e519ccef6e7c41cde16f09da
Lucas Ximenes <lucasdximenes@gmail.com>
2022-09-17 15:05:59 -0300

    finished styling Login page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={ store }>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { login } from '../redux/actions';
 import { getTriviaToken } from '../services/triviaAPI';
 import { saveTriviaToken } from '../services/localStorage';
+
+import logo from '../images/trivia-logo.png';
 
 class Login extends Component {
   state = {
@@ -31,55 +35,69 @@ class Login extends Component {
     const { history } = this.props;
     const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
     return (
-      <div>
+      <div className="flex flex-col items-center h-screen bg-zinc-900 text-white">
+        <img
+          src={ logo }
+          alt="trivia logo"
+          className="w-1/2 md:w-1/4 2xl:w-1/6 mt-16 mb-8"
+        />
         <form action="#">
-          <label htmlFor="email">
-            Email:
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-xl mb-2 font-bold">
+              Email:
+            </label>
             <input
               data-testid="input-gravatar-email"
+              className="w-48 h-10 mb-2 rounded-md text-black text-center font-bold focus:outline-none focus:ring-2 focus:ring-zinc-500"
               type="email"
               name="email"
               id="email"
               value={ email }
               onChange={ this.handleChange }
             />
-          </label>
 
-          <label htmlFor="name">
-            Name:
+            <label htmlFor="name" className="text-xl mb-2 font-bold">
+              Name:
+            </label>
             <input
               data-testid="input-player-name"
+              className="w-48 h-10 mb-2 rounded-md text-black text-center font-bold focus:outline-none focus:ring-2 focus:ring-zinc-500"
               type="text"
               name="name"
               id="name"
               value={ name }
               onChange={ this.handleChange }
             />
-          </label>
 
-          <button
-            data-testid="btn-play"
-            type="button"
-            disabled={ !emailRegex.test(email) || name.length < 1 }
-            onClick={ this.handleSubmit }
-          >
-            Play
-          </button>
+            <button
+              data-testid="btn-play"
+              className="w-48 h-10 mt-4 rounded-md bg-zinc-500 text-white font-bold hover:bg-zinc-600 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-zinc-500"
+              type="button"
+              disabled={ !emailRegex.test(email) || name.length < 1 }
+              onClick={ this.handleSubmit }
+            >
+              Play
+            </button>
+          </div>
         </form>
-        <button
-          type="button"
-          data-testid="btn-settings"
-          onClick={ () => history.push('/settings') }
-        >
-          Settings
-        </button>
-        <button
-          type="button"
-          data-testid="btn-ranking"
-          onClick={ () => history.push('/ranking') }
-        >
-          Ranking
-        </button>
+        <div className="flex">
+          <button
+            type="button"
+            data-testid="btn-settings"
+            className="w-24 h-10 mt-4 rounded-md bg-zinc-500 text-white font-bold hover:bg-zinc-600"
+            onClick={ () => history.push('/settings') }
+          >
+            Settings
+          </button>
+          <button
+            type="button"
+            data-testid="btn-ranking"
+            className="w-24 h-10 mt-4 ml-4 rounded-md bg-zinc-500 text-white font-bold hover:bg-zinc-600"
+            onClick={ () => history.push('/ranking') }
+          >
+            Ranking
+          </button>
+        </div>
       </div>
     );
   }
